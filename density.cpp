ad58cea19d9746b21576038b897748906a35878a
xenocaliver <xenocaliver@gmail.com>
2019-05-12 21:12:29 +0900

    Dr. wadayama's code are ported.

--- density.cpp
+++ density.cpp
@@ -15,7 +15,9 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>
 */
 #include <iostream>
+#include <cstdio>
 #include <cstdlib>
+#include <cinttypes>
 #include <cmath>
 #include <thread>
 #include <vector>
@@ -32,22 +34,223 @@ extern "C" {
 #include <fftw3.h>
 }
 #include "common.hpp"
-#include "fourier_gauss.hpp"
 #include "plot.hpp"
 
+fftw_plan p1,p2,p3;
+fftw_complex *_a, *_b, *_c, *_A, *_B, *_C;
+double *tmp1, *tmp2, *tmp3, *tmp4;
+std::vector<std::vector<int> > R_tbl(vector_size, std::vector<int>(vector_size, 0));
 namespace density {
 
-extern double* update_variable_node(std::vector<fftw_complex*>, fftw_complex*, uint64_t);
-extern double* update_check_node(std::vector<double*>, uint64_t);
+void clip_pdf(double* x) /* PDF clipping function */
+{
+  int i;
+  double sum;
 
-void evolution(std::vector<fftw_complex*> variable_node_inputs, fftw_complex* channel_dft_input, std::vector<double*> check_node_inputs, uint64_t vector_size, Plot* plot_app) {
-    uint64_t iteration;
-    double* ptr_double;
-    double probability;
+  sum = 0.0;
+  for (i = lower_bound + upper_bound; i < upper_bound + half_lower_bound; i++) {
+    sum += x[i];
+    x[i] = 0.0;
+  }
+  x[half_lower_bound + upper_bound] +=sum;
+  sum = 0.0;
+  for (i = upper_bound + half_upper_bound + 1; i < (int64_t)vector_size; i++) {
+    sum += x[i];
+    x[i] = 0.0;
+  }
+  x[half_upper_bound + upper_bound] +=sum;
+}
+
+double llr_pdf(double x, double channel_var) /* PDF of LLR (AWGN channel) */
+{
+  double var = 4.0/channel_var;
+  double mean = 2.0/channel_var;
+  return (1.0/sqrt(2.0*pi*var)) * exp(-(x-mean)*(x-mean)/(2.0*var));
+}
+
+void quantize_pdf(double* a, double channel_var) /* The function makes quatized version of a given PDF function.  */
+{
+  int i;
+  double l,r;
+  double vl,vr;
+
+  for (i = half_lower_bound; i < half_upper_bound; i++) {
+    l = delta*(double)i - 0.5*delta;
+    r = delta*(double)i + 0.5*delta;
+    vl = llr_pdf(l, channel_var);
+    vr = llr_pdf(r, channel_var);
+    a[i + upper_bound] = (vl + vr)*0.5;
+  }
+  for (i = lower_bound; i < half_lower_bound; i++) a[i + upper_bound] = 0.0;
+  for (i = half_upper_bound + 1; i < upper_bound; i++) a[i + upper_bound] = 0.0;
+}
+
+double error_prob(double* a)
+{
+  int i;
+  double sum;
+
+  sum = 0.0;
+  for (i = 0; i < upper_bound; i++) {
+    sum +=  a[i];
+  }
+  sum += 0.5*a[upper_bound];
+  return(sum*delta);
+}
+
+void normalize_pdf(double* r)
+{
+  int i;
+  double sum;
+
+  sum = 0.0;			/* normalization */
+
+  for (i = 0; i < (int64_t)vector_size; i++) sum += r[i]*delta;
+  for (i = 0; i < (int64_t)vector_size; i++) r[i]/= sum;
+}
+
+void conv(double* a, double* b, double* r) /* convolution of two PDFs */
+{
+  int i;
+
+  for (i = 0; i < (int64_t)vector_size; i++) {
+    _a[i][0] = a[i];
+    _a[i][1] = 0.0;
+    _b[i][0] = b[i];
+    _b[i][1] = 0.0;
+  }
+  fftw_execute(p1);
+  fftw_execute(p2);
+  for (i = 0; i < (int64_t)vector_size; i++) {
+    _C[i][0] = _A[i][0]*_B[i][0]-_A[i][1]*_B[i][1];
+    _C[i][1] = _A[i][0]*_B[i][1]+_A[i][1]*_B[i][0];
+  }
+  fftw_execute(p3);
+  for (i = 0; i < (int64_t)vector_size; i++) {
+    r[i] = _c[((vector_size - 1)/2 + i)%vector_size][0]*delta/(double)vector_size;
+  }
+  clip_pdf(r);
+}
+
+void delta_func(double* a) /* delta function */
+{
+  int i;
+  for (i = 0; i < (int64_t)vector_size; i++) {
+    a[i] = 0.0;
+  }
+  a[upper_bound] = 1.0/delta;
+}
+
+void naive_convpower(double* a, double* b, int n)
+{
+  int i;
+  for (i = 0; i < (int64_t)vector_size; i++) {
+    b[i] = a[i];
+  }
+  for (i = 1; i < n; i++) {
+    conv(a,b,b);
+  }
+}
+
+void convpower(double* a, double* b, int n)
+{
+  int i;
+  int w;
+  int bit;
+
+  if (n < 6) {
+    naive_convpower(a, b, n);
+    return;
+  }
+  for (i = 0; i < (int64_t)vector_size; i++) tmp1[i] = a[i];
+  delta_func(b);
+  w = 0;
+  i = 0;
+  while(true) {
+    bit = (n >> i) & 1;
+    if (bit == 1) conv(b, tmp1, b);
+    if (bit == 1) w += (1<<i);
+    if (w == n) break;
+    i++;
+    conv(tmp1, tmp1, tmp1);
+  }
+}
+
+double R(double a, double b)
+{
+  return 2.0 * atanh(tanh(0.5*a)*tanh(0.5*b));
+}
+
+void init() /* initialization */
+{
+  int i,j;
+  int idx = 0;
+  double a,b,v;
+
+  printf("#vector_size      = %" PRIu64 "\n", vector_size);
+  printf("#lower_bound      = %" PRId64 "\n", lower_bound);
+  printf("#upper_bound      = %" PRId64 "\n", upper_bound);
+  printf("#half_lower_bound = %" PRId64 "\n", half_lower_bound);
+  printf("#half_upper_bound = %" PRId64 "\n", half_upper_bound);
+
+  _a = (fftw_complex *)fftw_malloc(sizeof(fftw_complex)*vector_size);
+  _b = (fftw_complex *)fftw_malloc(sizeof(fftw_complex)*vector_size);
+  _c = (fftw_complex *)fftw_malloc(sizeof(fftw_complex)*vector_size);
+  _A = (fftw_complex *)fftw_malloc(sizeof(fftw_complex)*vector_size);
+  _B = (fftw_complex *)fftw_malloc(sizeof(fftw_complex)*vector_size);
+  _C = (fftw_complex *)fftw_malloc(sizeof(fftw_complex)*vector_size);
+
+  tmp1 = (double *)fftw_malloc(sizeof(double)*vector_size);
+  tmp2 = (double *)fftw_malloc(sizeof(double)*vector_size);
+  tmp3 = (double *)fftw_malloc(sizeof(double)*vector_size);
+  tmp4 = (double *)fftw_malloc(sizeof(double)*vector_size);
+	
+  for (i = half_lower_bound; i <= half_upper_bound; i++) {
+    for (j = half_lower_bound; j <= half_upper_bound; j++) {
+      a = delta*i;
+      b = delta*j;
+      v = R(a,b);
+      if (v >= 0.5*delta) idx = (int)floor(v/delta + 0.5);
+      if (v <= -0.5*delta) idx = (int)ceil(v/delta - 0.5);
+      if ((v < 0.5*delta) && (v > -0.5*delta)) idx = 0;
+      if (v > half_upper_bound*delta) idx = half_upper_bound;
+      if (v < half_lower_bound*delta) idx = half_lower_bound;
+      R_tbl[i + upper_bound][j + upper_bound] = idx;
+    }
+  }
+  p1 = fftw_plan_dft_1d(vector_size, _a, _A, FFTW_FORWARD, FFTW_ESTIMATE);  
+  p2 = fftw_plan_dft_1d(vector_size, _b, _B, FFTW_FORWARD, FFTW_ESTIMATE);  
+  p3 = fftw_plan_dft_1d(vector_size, _C, _c, FFTW_BACKWARD, FFTW_ESTIMATE);  
+}
+
+void apply_R(double* a, double* b, double* c)
+{
+  int i,j;
+
+  for (i = 0; i < (int64_t)vector_size; i++) tmp1[i] = 0.0;
+  for (i = half_lower_bound + upper_bound; i <= half_upper_bound + upper_bound; i++) {
+    for (j = half_lower_bound + upper_bound; j <= half_upper_bound + upper_bound; j++) {
+      tmp1[R_tbl[i][j] + upper_bound ] += a[i]*b[j];
+    }
+  }
+  for (i = 0; i < (int64_t)vector_size; i++) c[i] = tmp1[i]*delta;
+}
+
+void Rpower(double* a, double* b, int n)
+{
+  int i;
+  for (i = 0; i <(int64_t)vector_size; i++) b[i] = a[i];
+  for (i = 1; i < n; i++) apply_R(a,b,b);
+}
+
+void evolution(uint64_t vector_size, uint64_t variable_node_degree, uint64_t check_node_degree, double channel_var, Plot* plot_app) {
     double* x;
-    uint64_t uli, ulj;
+    double probability, probability_old;
+    double *P_lambda, *P_c2m, *P_m2c, *f0, *f1;
+    double *tmpA;
+    double *tmpB;
+    uint64_t iteration;
     int64_t i;
-    std::vector<fftw_complex*> v(variable_node_inputs.size() + 1, nullptr);
 
     x = (double *)fftw_malloc(sizeof(double)*vector_size);
     if(x == nullptr) {
@@ -56,83 +259,84 @@ void evolution(std::vector<fftw_complex*> variable_node_inputs, fftw_complex* ch
     }
     for(i = 0; i < (int64_t)vector_size; i++) x[i] = (double)(i - upper_bound)*delta;
 
-    for(uli = 0; uli < v.size(); uli++) {
-        v[uli] = (fftw_complex *)fftw_malloc(sizeof(fftw_complex)*vector_size);
-        if(v[uli] == nullptr) {
-            std::cerr << "Can not allocate memory." << std::endl;
-            exit(-1);
-        }
-    }
+    init();
+
+    P_lambda = (double *)fftw_malloc(sizeof(double)*vector_size);
+    P_c2m = (double *)fftw_malloc(sizeof(double)*vector_size);
+    P_m2c = (double *)fftw_malloc(sizeof(double)*vector_size);
+    f0 = (double *)fftw_malloc(sizeof(double)*vector_size);
+    f1= (double *)fftw_malloc(sizeof(double)*vector_size);
+    tmpA = (double *)fftw_malloc(sizeof(double)*vector_size);
+    tmpB = (double *)fftw_malloc(sizeof(double)*vector_size);
+
+	/* initialization of LLR density */
+    quantize_pdf(P_lambda, channel_var);
+    normalize_pdf(P_lambda);
+
+	/* initialization of message(c->m) density */
+    delta_func(P_c2m);
+
+    probability_old = 1.0;
 
     for(iteration = 0; iteration < 1000; iteration++) {
-    /*variable node calculation */
-        ptr_double = density::update_variable_node(variable_node_inputs, channel_dft_input, vector_size);
-    
-    /* extract real part */
-        normalize_pdf(ptr_double, vector_size);
+	    /* variable node operation */
+        convpower(P_c2m, tmpA, variable_node_degree - 1);
+        conv(P_lambda, tmpA, P_m2c);
+        normalize_pdf(P_m2c);
 
-    /* copy probability distribution */
-        for(uli = 0; uli < 5; uli++) std::memcpy(check_node_inputs[uli], ptr_double, sizeof(double)*vector_size);
-
-    /* free memrmoy */
-        fftw_free(ptr_double);
-
-    /* check node calculation */
-        ptr_double = density::update_check_node(check_node_inputs, vector_size);
-        density::normalize_pdf(ptr_double, vector_size);
-
-    /* copy memory */
-        for(uli = 0; uli < 2; uli++) {
-            for(ulj = 0; ulj < vector_size; ulj++) {
-                variable_node_inputs[uli][ulj][0] = ptr_double[ulj];
-                variable_node_inputs[uli][ulj][1] = (double)0.0;
-            }
-        }
-        fftw_free(ptr_double);
-        for(uli = 0; uli < v.size(); uli++) std::memcpy(v[uli], variable_node_inputs[0], sizeof(fftw_complex)*vector_size);
-    /* calculate joint probability */
-        ptr_double = density::update_variable_node(v, channel_dft_input, vector_size);
-    
-    /* extract real part */
-        normalize_pdf(ptr_double, vector_size);
-
-    /* caculating joint probability */
-        probability = density::get_error_probability(ptr_double);
-        std::cout << std::setw(8) << probability << std::endl;
-
-    /* update graph */
-        plot_app->updateCurve(x, ptr_double, vector_size);
+        /* update graph */
+        plot_app->updateCurve(x, P_m2c, vector_size);
         plot_app->emitSignal();
+        
+		/* BER computation */
+        probability = error_prob(P_m2c);
+        std::printf("# %" PRIu64 " %16.12f\n", iteration, probability);
 
-        fftw_free(ptr_double);
+        if (fabs(probability-probability_old) < 1e-5) break; /* break condition */
+
+        probability_old = probability;
+		/* check node operation */ 
+        Rpower(P_m2c, P_c2m, check_node_degree - 1);
+        normalize_pdf(P_c2m);
     }
-    for(uli = 0; uli < 2; uli++) fftw_free(variable_node_inputs[uli]);
-    for(uli = 0; uli < 5; uli++) fftw_free(check_node_inputs[uli]);
-    for(uli = 0; uli < v.size(); uli++) fftw_free(v[uli]);
     fftw_free(x);
+    fftw_free(P_lambda);
+    fftw_free(P_c2m);
+    fftw_free(P_m2c);
+    fftw_free(f0);
+    fftw_free(f1);
+    fftw_free(tmpA);
+    fftw_free(tmpB);
+    fftw_free(_a);
+    fftw_free(_b);
+    fftw_free(_c);
+    fftw_free(_A);
+    fftw_free(_B);
+    fftw_free(_C);
+    fftw_free(tmp1);
+    fftw_free(tmp2);
+    fftw_free(tmp3);
+    fftw_free(tmp4);
 }
 
 }
 
 int main(int argc, char* argv[]) {
-    uint64_t uli;
-    std::vector<fftw_complex*> variable_node_inputs(2, nullptr);
-    std::vector<double*> check_node_inputs(5, nullptr);
-    fftw_complex* channel_input = (fftw_complex *)fftw_malloc(sizeof(fftw_complex)*vector_size);
-    fftw_complex* channel_dft_input = (fftw_complex *)fftw_malloc(sizeof(fftw_complex)*vector_size);
-    std::vector<double> initial_distribution;
-    std::vector<double> delta_distribution;
-    double sigma = 0.15;
-    fftw_complex* ptr_input;
-    fftw_plan plan;
-    double* ptr_double;
     QApplication app(argc, argv);
-    Plot* delta_plot = new Plot();
-    Plot* awgn_plot = new Plot();
     Plot* probability_plot = new Plot();
     double* x;
     std::string curve_name;
     int64_t i;
+    uint64_t variable_node_degree, check_node_degree;
+    double sigma;
+
+    if(argc != 4) {
+        std::cerr << "Usage: ./density <variable node degree> <check node degree> <sigma>" << std::endl;
+        return(EXIT_FAILURE);
+    }
+    variable_node_degree = std::strtoul(argv[1], nullptr, 10);
+    check_node_degree = std::strtoul(argv[2], nullptr, 10);
+    sigma = std::stod(argv[3]);
 
     x = (double *)fftw_malloc(sizeof(double)*vector_size);
     if(x == nullptr) {
@@ -140,55 +344,6 @@ int main(int argc, char* argv[]) {
         return(EXIT_FAILURE);
     }
     for(i = 0; i < (int64_t)vector_size; i++) x[i] = (double)(i - upper_bound)*delta;
-
-    for(uli = 0; uli < 2; uli++) {
-        ptr_input = (fftw_complex *)fftw_malloc(sizeof(fftw_complex)*vector_size);
-        variable_node_inputs[uli] = ptr_input;
-    }
-    for(uli = 0; uli < 5; uli++) {
-        ptr_double = (double *)fftw_malloc(sizeof(double)*vector_size);
-        check_node_inputs[uli] = ptr_double;
-    }
-
-    /* initial distribution */
-    initial_distribution = density::quantize_pdf(sigma);
-    density::normalize_pdf(initial_distribution.data(), initial_distribution.size());
-    for(uli = 0; uli < vector_size; uli++) {
-        channel_input[uli][0] = initial_distribution[uli];
-        channel_input[uli][1] = (double)0.0;
-    }
-
-    plan = fftw_plan_dft_1d(vector_size, channel_input, channel_dft_input, FFTW_FORWARD, FFTW_ESTIMATE);
-    fftw_execute(plan);
-    fftw_destroy_plan(plan);
-
-    delta_distribution = density::get_delta_function();
-    density::normalize_pdf(delta_distribution.data(), delta_distribution.size());
-    for(uli = 0; uli < vector_size; uli++) {
-        variable_node_inputs[0][uli][0] = delta_distribution[uli];
-        variable_node_inputs[0][uli][1] = (double)0.0;
-    }
-
-    // draw input delta function
-    delta_plot->resize(800, 600);
-    delta_plot->setTitle("delta distribution");
-    curve_name = R"(delta function)";
-    delta_plot->setupCurve(curve_name);
-    delta_plot->setAxisScale(0, 0.0, 1.2, 0);
-    delta_plot->setAxisScale(2, x[0], x[vector_size - 1], 0);
-    delta_plot->updateAxes();
-    delta_plot->plotCurve(x, delta_distribution.data(), vector_size);
-    delta_plot->show();
-
-    // draw input AWGN pdf
-    awgn_plot->resize(800, 600);
-    awgn_plot->setTitle("AWGN noise distribution");
-    curve_name = R"(AWGN)";
-    awgn_plot->setupCurve(curve_name);
-    awgn_plot->setAxisScale(2, x[0], x[vector_size - 1], 0);
-    awgn_plot->updateAxes();
-    awgn_plot->plotCurve(x, initial_distribution.data(), vector_size);
-    awgn_plot->show();
 
     // draw probability density function 
     probability_plot->resize(800, 600);
@@ -200,9 +355,7 @@ int main(int argc, char* argv[]) {
     QObject::connect(probability_plot, SIGNAL(emitSignal()), probability_plot, SLOT(replot()));
     probability_plot->show();
 
-    std::memcpy(variable_node_inputs[1], variable_node_inputs[0], sizeof(fftw_complex)*vector_size);
-
-     std::thread evo_thread(density::evolution, variable_node_inputs, channel_dft_input, check_node_inputs, vector_size, probability_plot);
+     std::thread evo_thread(density::evolution, vector_size, variable_node_degree, check_node_degree, sigma*sigma, probability_plot);
      evo_thread.detach();
 
     return(app.exec());
